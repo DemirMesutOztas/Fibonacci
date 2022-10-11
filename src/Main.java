72831192ed2e07a60242eada6cf0bdf63ad8a432
Gogle Öztaş <demiroztas06@hotmail.com>
2022-10-11 15:24:34 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.print("Sayı giriniz: ");
+
+        Scanner input = new Scanner(System.in);
+        int counter = input.nextInt();
+        int num1=0;
+        int num2=1;
+        boolean status=true;
+        int result=0;
+
+        while(counter>1)
+        {
+            if(status)
+            {
+                System.out.print(num1+" "+num2+" ");
+                status=false;
+
+            }
+            else
+            {
+                result=num1+num2;
+                num1=num2;
+                num2=result;
+                System.out.print(result+" ");
+                counter--;
+            }
+
+        }
     }
 }
